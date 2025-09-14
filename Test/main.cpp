02b819bf664ffd93f1536298887135dfedfb0684
Rasmus Anthin <scratty@gmail.com>
2025-09-15 00:04:41 +0200

    main.cpp: Printing the device name.

--- Test/main.cpp
+++ Test/main.cpp
@@ -12,6 +12,7 @@ int main(int argc, const char* argv[])
 {
   // insert code here...
   applaudio::AudioEngine engine;
+  engine.print_device_name();
   std::cout << "Hello, World!\n";
   return 0;
 }
