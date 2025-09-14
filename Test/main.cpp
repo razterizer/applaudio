8c64b7df8d287973b1e476b16d6f089ea8537c0b
Rasmus Anthin <scratty@gmail.com>
2025-09-14 21:45:50 +0200

    Added (mistakenly added file in a former commit because xcode is a bit stupid) Test/main.cpp for testing of the audio library.

--- Test/main.cpp
+++ Test/main.cpp
@@ -6,9 +6,12 @@
 //
 
 #include <iostream>
+#include <applaudio/AudioEngine.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[])
+{
   // insert code here...
+  applaudio::AudioEngine engine;
   std::cout << "Hello, World!\n";
   return 0;
 }
