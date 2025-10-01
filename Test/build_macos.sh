b28746314d5a477725895e11f5e03b5eb367513e
Rasmus Anthin <scratty@gmail.com>
2025-10-01 18:42:24 +0200

    Removing dependency to Core in Test/build_macos.sh build script.

--- Test/build_macos.sh
+++ Test/build_macos.sh
@@ -5,4 +5,4 @@
 #  Created by Rasmus Anthin on 2025-09-18.
 #
 
-g++ test.cpp -o test -std=c++20 -I../include -I../../Core/include -framework AudioToolbox -framework CoreAudio -framework CoreFoundation
+g++ test.cpp -o test -std=c++20 -I../include -framework AudioToolbox -framework CoreAudio -framework CoreFoundation
