0d174845d6ebae766cc102ae39ddb34f93e1fe8e
Rasmus Anthin <scratty@gmail.com>
2025-10-01 18:39:31 +0200

    Added new header System.h with is_wsl() copied from the Core repo. * It uses the StringUtils.h header to unify the result from /proc/version. Just like the one in Core/System.h.

---
+++ include/applaudio/System.h
@@ -0,0 +1,38 @@
+//
+//  System.h
+//  applaudio
+//
+//  Created by Rasmus Anthin on 2025-10-01.
+//
+
+#pragma once
+#include "StringUtils.h"
+#include <fstream>
+
+namespace sys
+{
+
+  bool is_wsl()
+  {
+#ifdef __linux__
+    static bool checked = false;
+    static bool wsl_flag = false;
+    
+    if (checked)
+      return wsl_flag;
+    std::ifstream version_file("/proc/version");
+    std::string version;
+    if (version_file && std::getline(version_file, version))
+    {
+      auto version_lower = str::to_lower(version);
+      //std::cout << version << std::endl;
+      wsl_flag = version_lower.find("microsoft") != std::string::npos
+              || version_lower.find("wsl") != std::string::npos;
+    }
+    checked = true;
+    return wsl_flag;
+#endif
+    return false;
+  }
+
+}
