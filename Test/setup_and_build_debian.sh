68ad6e781fe2b53c46c9100e9ddfd8dbf7b615d7
Rasmus Anthin <scratty@gmail.com>
2025-10-02 18:57:16 +0200

    Added a setup_and_build_debian.sh script which installs ALSA dev pack and pkg-config and then runs the build script as normal.

---
+++ Test/setup_and_build_debian.sh
@@ -0,0 +1,12 @@
+#
+#  setup_and_build_macos.sh
+#  applaudio
+#
+#  Created by Rasmus Anthin on 2025-09-18.
+#
+
+sudo apt install pkg-config
+
+sudo apt install libasound2-dev
+
+./build_debian.sh
