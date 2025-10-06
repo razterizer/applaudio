3eb1be30b287ef9f3bb2718074c709d62521c128
Rasmus Anthin <scratty@gmail.com>
2025-10-06 07:21:01 +0200

    Source.h: Prevents "tearing" on playing bool variable by making it atomic.

--- include/applaudio/Source.h
+++ include/applaudio/Source.h
@@ -7,6 +7,7 @@
 
 #pragma once
 #include "Object3D.h"
+#include <atomic>
 
 namespace applaudio
 {
@@ -17,7 +18,7 @@ namespace applaudio
     bool looping = false;
     float volume = 1.0f;
     float pitch = 1.0f;
-    bool playing = false;
+    std::atomic<bool> playing = false;
     double play_pos = 0;
     
     a3d::Object3D object_3d;
