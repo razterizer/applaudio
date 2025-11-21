54d147499e848270e102d5f3116089eaeb09e646
Rasmus Anthin <scratty@gmail.com>
2025-11-21 22:12:47 +0100

    System.h: Improved version of is_wsl() that is thread-safe and free of the risk of race-conditions due to the static bools before.

--- include/applaudio/System.h
+++ include/applaudio/System.h
@@ -15,24 +15,23 @@ namespace apl_sys
   bool is_wsl()
   {
 #ifdef __linux__
-    static bool checked = false;
-    static bool wsl_flag = false;
-    
-    if (checked)
-      return wsl_flag;
-    std::ifstream version_file("/proc/version");
-    std::string version;
-    if (version_file && std::getline(version_file, version))
+    static const bool result = []()
     {
-      auto version_lower = apl_str::to_lower(version);
-      //std::cout << version << std::endl;
-      wsl_flag = version_lower.find("microsoft") != std::string::npos
-              || version_lower.find("wsl") != std::string::npos;
-    }
-    checked = true;
-    return wsl_flag;
-#endif
+      std::ifstream version_file("/proc/version");
+      std::string version;
+      
+      if (!version_file || !std::getline(version_file, version))
+        return false;
+      
+      auto version_lower = str::to_lower(version);
+      return version_lower.find("microsoft") != std::string::npos
+          || version_lower.find("wsl") != std::string::npos;
+    }();
+    
+    return result;
+#else
     return false;
+#endif
   }
 
 }
